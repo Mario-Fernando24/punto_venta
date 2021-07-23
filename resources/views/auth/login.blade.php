3e545daf58e62fa9d265eb579c67819336384e38
Mario Muñoz <6872914-mariomunoz78@users.noreply.gitlab.com>
2021-07-22 22:39:28 -0500

    add img in logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,11 +10,12 @@
           @csrf
  
             <div class="card-body">
-              <h1>TenderPos</h1>
-              <p class="text-muted">Control de acceso a TenderPos</p>
+              <br>
+              <h2>TenderPos Control</h2>
+              <br>
               <div class="input-group mb-3 @error('usuario') is-invalid @enderror">
                 <span class="input-group-addon"><i class="icon-user"></i></span>
-                <input type="text" name="usuario" value="{{old('usuario')}}"  id="usuario" class="form-control" placeholder="Usuario">
+                <input type="text" name="usuario" value="{{ old('usuario')}}"  id="usuario" class="form-control" placeholder="Usuario">
               </div>
               @if ($errors->has('usuario'))
                <span class="help-block">
@@ -44,22 +45,20 @@
                 <div class="col-12">
                   <input type="submit" value="Acceder" class="btn  btn-outline-primary btn-block">
                 </div>
-                <div class="col-6 text-right">
+                <!-- <div class="col-6 text-right">
                   <button type="button" class="btn btn-link px-0">Olvidaste tu password?</button>
-                </div>
+                </div> -->
               </div>
             </div>
             </form>
           </div>
-          <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
+          <div class="card text-white bg-grey py-5 d-md-down-none" style="width:44%">
             <div class="card-body text-center">
               <div>
                 <h2> TenderPos</h2>
-                <p><b>TenderPos</b> no solo te ayudará a agilizar tus ventas, 
-                también te aportará la opción de poder controlar tu inventario en tiempo real.
-                 De esta forma, evitas hacer un conteo manual o tener que trasladarte físicamente a tu 
-                 almacén para conocer la cantidad de productos disponibles.</p>
-                <a href="" target="_blank" class="btn btn-primary active mt-3">subliowen</a>
+                <img src="img/tenderpos.jpeg" heigt="130px" width="250px" class="col-xs-12"></p>
+
+                <!-- <a href="" target="_blank" class="btn btn-primary active mt-3">TenderPos</a> -->
               </div>
             </div>
           </div>
